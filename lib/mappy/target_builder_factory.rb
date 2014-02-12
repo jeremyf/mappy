ab166bedfaf9f61d5fff327d07280ce3e9935cb4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-12 10:43:09 -0500

    Creating TargetBuilderFactory
    Responsible for taking a symbol, finding the appropriate class then
    verifying that the class is "well formed."

--- lib/mappy/target_builder_factory.rb
+++ lib/mappy/target_builder_factory.rb
@@ -1,10 +1,35 @@
 require 'active_support/inflector'
 
 module Mappy
+  class InvalidTargetBuilder < RuntimeError
+  end
+
+  # Responsible for taking a symbol, finding the appropriate class then
+  # verifying that the class is "well formed."
   class TargetBuilderFactory
     def call(target_symbol)
-      target_class = target_symbol.to_s.classify.constantize
-      target_class.method(:new)
+      target_builder = extract_target_builder(target_symbol)
+      validate!(target_builder)
+      target_builder
+    end
+
+    private
+    def extract_target_builder(target_symbol)
+      target_symbol.to_s.classify.constantize
+    end
+
+    def validate!(target_builder)
+      target_instance = target_builder.allocate
+      if ! target_instance.respond_to?(:attributes=)
+        raise InvalidTargetBuilder.new("Expected an instance of #{target_builder} to have #attributes= method")
+      end
+
+      begin
+        target_instance.send(:initialize, {})
+      rescue ArgumentError => e
+        raise InvalidTargetBuilder.new("Expected #{target_builder} to initialize with an attributes hash.\n#{e}")
+      end
+
     end
   end
 end
--- spec/lib/mappy/target_builder_factory_spec.rb
+++ spec/lib/mappy/target_builder_factory_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path('../../../../lib/mappy/target_builder_factory', __FILE__)
 
 module Mappy
-  class MockModel
+  class MockValidModel
+    attr_accessor :attributes
+    def initialize(attributes)
+    end
+  end
+
+  class MockNoAttributesModel
+  end
+
+  class MockImproperInitializeMethodSignatureModel
+    attr_accessor :attributes
   end
 
   describe TargetBuilderFactory do
@@ -9,7 +19,19 @@ module Mappy
 
     context '#call' do
       it 'returns a builder lambda based on the input' do
-        expect(subject.call('mappy/mock_model')).to respond_to(:call)
+        expect(subject.call('mappy/mock_valid_model')).to respond_to(:new)
+      end
+
+      it 'raises an exception when the builder does not have attributes' do
+        expect {
+          subject.call('mappy/mock_no_attributes_model')
+        }.to raise_error
+      end
+
+      it 'raises an exception when the builder initialize does not take a hash' do
+        expect {
+          subject.call('mappy/mock_improper_initialize_method_signature_model')
+        }.to raise_error
       end
     end
   end
