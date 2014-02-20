940725a07d690f20431b313be71ff0e8538a24d8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-19 20:30:02 -0500

    Adding documentation to Mappy's main functions

--- lib/mappy.rb
+++ lib/mappy.rb
@@ -4,45 +4,65 @@ require "mappy/configuration"
 module Mappy
   class << self
     attr_writer :configuration
+    def configuration
+      @configuration ||= Configuration.new
+    end
+    private :configuration
+
   end
 
   module_function
-
+  # Determines the object's Mappy type
+  #
+  # @param object [Object] the source object
+  # @return [String] the Mappy type which should be used in configuring Mappy
+  # @see Mappy.configure
   def to_type(object)
     return object.to_mappy_type if object.respond_to?(:to_mappy_type)
     object.class.to_s.underscore
   end
 
-  # Used for configuring available RemoteService and any additional
-  # initialization requirements for those RemoteServices (i.e. credentials)
+  # Registers a collection of maps.
   #
   # @example
+  #   Mappy.configure do |config|
+  #     config.register(source: 'book', target: 'orcid/work', legend: [:name, :title])
+  #   end
   #
   # @yieldparam config [Configuration]
   # @see Mappy::Railtie
   def configure(&block)
-    @configuration_block = block
-
-    # The Rails load sequence means that some of the configured Targets may
-    # not be loaded; As such I am not calling configure! instead relying on
-    # Mappy::Railtie to handle the configure! call
-    finalize! unless defined?(Rails)
+    yield(configuration)
   end
 
+  # Converts the source object to an instance of the options[:target].
+  #
+  # @example
+  #   Mappy.configure do |config|
+  #     config.register(source: 'book', target: 'orcid/work', legend: [:name, :title])
+  #   end
+  #   book = Book.new(title: 'Hello World')
+  #   orcid_work = Mappy.map(book, target: 'orcid/work')
+  #
+  #   assert_equal orcid_work.title, book.name
+  #
+  # @param source [Object] :source - the instance of a class that you are mapping from
+  # @param options [Hash]
+  # @options options [String] :target - the class name of the target that you are mapping to.
+  # @return [Object] an instance of the :target that has been instantiated by mapping the :source object
+  #
+  # @see Mappy.configure
   def map(source, options = {})
     configuration.map(source, options)
   end
 
-  def configuration
-    @configuration ||= Configuration.new
-  end
-
+  # @deprecated
   def finalize!
-    if @configuration_block.respond_to?(:call)
-      @configuration_block.call(configuration)
-    end
   end
 
+  # Because sometimes, when you are testing things, you might want a new
+  # configuration.
+  # :nodoc:
   def reset!
     self.configuration = Configuration.new
   end
